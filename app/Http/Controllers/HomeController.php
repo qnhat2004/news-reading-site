12205c4b9f2fca585f78f903585421115bc99c9f
Vũ Quang Nhật <nhatchien12@gmail.com>
2024-12-04 13:16:51 +0700

    add search

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,15 +22,27 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        $news = News::latest()->paginate(10);
-        return view('home.index', compact('news'));
-    }
+    // public function index()
+    // {
+    //     $news = News::latest()->get();
+    //     return view('home.index', compact('news'));
+    // }
 
     public function show($id)
     {
         $news = News::findOrFail($id);
         return view('news.detail', compact('news'));
+    }
+
+    public function index(Request $request)
+    {
+        $query = $request->input('search');
+        if ($query) {
+            $news = News::where('title', 'like', "%{$query}%")->get();  // Tìm kiếm theo tiêu đề
+        } else {
+            $news = News::latest()->get();  // Lấy tất cả nếu không tìm kiếm
+        }
+
+        return view('home.index', compact('news'));
     }
 }
