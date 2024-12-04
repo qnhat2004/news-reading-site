3d996225a0af484aa8809f6f55242298436338f0
Vũ Quang Nhật <nhatchien12@gmail.com>
2024-12-04 14:21:22 +0700

    add the date added to card

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -10,12 +10,15 @@
         @foreach($news as $item)
             <div class="col-md-4 mb-4">
                 <div class="card">
-                    <img src="{{ $item->image }}" class="card-img-top" alt="{{ $item->title }}">
+                    <img src="{{ $item->image }}" class="card-img-top" alt="{{ $item->title }}" style="height: 400px; object-fit: cover;">
                     <div class="card-body">
-                        <h5 class="card-title">{{ $item->title }}</h5>
+                        <h5 class="card-title" style="font-size: x-large;">{{ $item->title }}</h5>
                         <p class="card-text">{{ \Illuminate\Support\Str::limit($item->content, 100) }}</p>
                         <a href="{{ route('news.detail', $item->id) }}" class="btn btn-primary">Xem chi tiết</a>
                     </div>
+                <div class="card-footer text-muted">
+                    Đăng ngày: {{ $item->created_at->format('d/m/Y') }}
+                </div>
                 </div>
             </div>
         @endforeach
