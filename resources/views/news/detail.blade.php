7840dd49ce932a34e1b46f3a0bf8959fa085ad08
Vũ Quang Nhật <nhatchien12@gmail.com>
2024-12-04 22:13:17 +0700

    update detail

--- resources/views/news/detail.blade.php
+++ resources/views/news/detail.blade.php
@@ -8,8 +8,17 @@
                 <div class="card-header"><h3>{{ $news->title }}</h3></div>
 
                 <div class="card-body">
-                    <img src="{{ $news->image }}" class="img-fluid mb-4" alt="{{ $news->title }}">
-                    <p>{{ $news->content }}</p>
+                    <div class="row">
+                        <div class="col-md-2">
+                            <img src="{{ $news->image }}" class="img-fluid mb-4" alt="{{ $news->title }}">
+                        </div>
+                        <div class="col-md-6">
+                            <p>{{ $news->content }}</p>
+                        </div>
+                        <div class="text-muted">
+                            Đăng ngày: {{ $news->created_at->format('d/m/Y') }}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
